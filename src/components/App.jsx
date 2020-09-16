200b8499b027c4d04d66a7cdbf5d4df1402dc5ed
swissnez <neal@abovebeyondtech.se>
2020-09-16 19:22:45 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function App() {
+  // contact = var & setContact = ()
+  const [contact, setContact] = useState({ // Object here
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event){
+    const {name,value} = event.target; // declare {} from event obj
+    setContact({
+      fName: ("fName" === name) ? value: contact.fName,
+      lName: ("lName" === name) ? value: contact.lName,
+      email: ("email" === name) ? value: contact.email
+    });
+
+  }
+
+
+ 
+  console.log((contact.fName) && (contact.lName) && (contact.email) ? contact:"");
+ 
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form onChange={handleChange}>
+        <input name="fName" placeholder="First Name"  />
+        <input name="lName" placeholder="Last Name" />
+        <input name="email" placeholder="Email" />
+        <button method="post" action="/submitRoute" name="myButton">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
